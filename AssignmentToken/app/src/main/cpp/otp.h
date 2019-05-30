cecd99d40b3baadb2b454555ae04b5fcb1aa74ca
Anderson <atflourenco@gmail.com>
2019-05-30 11:55:30 -0300

    Add extern libraries in project to generate a hash.

---
+++ AssignmentToken/app/src/main/cpp/otp.h
@@ -0,0 +1,17 @@
+//
+// Created by Anderson on 30/05/2019.
+//
+
+//#ifndef ASSIGNMENTTOKEN_OTP_H
+//#define ASSIGNMENTTOKEN_OTP_H
+//
+//#endif //ASSIGNMENTTOKEN_OTP_H
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void generateotp(char *key, int key_length, char *digest);
+
+#ifdef __cplusplus
+}
+#endif
