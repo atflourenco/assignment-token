80e6dd0731e8f92c088ad3aff9d30420b9a0d5af
Anderson <atflourenco@gmail.com>
2019-05-30 17:24:28 -0300

    Remove import unutilized

--- AssignmentToken/app/src/main/java/com/token/dao/DataDao.java
+++ AssignmentToken/app/src/main/java/com/token/dao/DataDao.java
@@ -1,7 +1,6 @@
 package com.token.dao;
 
 import android.arch.persistence.room.Dao;
-import android.arch.persistence.room.Delete;
 import android.arch.persistence.room.Insert;
 import android.arch.persistence.room.Query;
 
